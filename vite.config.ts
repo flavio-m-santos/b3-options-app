e161381b64195040174a1c4384235b61b2159911
Flavio Miguel <85520095+flavio-m-santos@users.noreply.github.com>
2025-12-27 02:25:05 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -9,6 +8,6 @@ export default defineConfig({
     outDir: 'dist',
   },
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || "")
   }
 })
